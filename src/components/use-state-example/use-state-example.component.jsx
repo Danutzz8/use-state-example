163a89bee329b1effde75ea7ee59171fbbc4e1f2
Danutzz8 <Marius.Prutean@outlook.com>
2021-02-08 22:54:05 +0200

    name changed

--- src/components/use-state-example/use-state-example.component.jsx
+++ src/components/use-state-example/use-state-example.component.jsx
@@ -3,18 +3,20 @@ import React, { useState } from 'react';
 import Card from '../card/card.component';
 
 const UseStateExample = () => {
-  const [name, setName] = useState('Yihua');
-  const [address, setAddress] = useState('Amsterdam');
+  const [name, setName] = useState('Marius');
+  const [address, setAddress] = useState('London');
 
   return (
     <Card>
       <h1> {name} </h1>
       <h1> {address} </h1>
-      <button onClick={() => setName('Andrei')}>Set Name to Andrei</button>
-      <button onClick={() => setAddress('Canada')}>Set Address</button>
+      <button onClick={() => setName('Danutzz')}>Set Name to Danutzz</button>
+      <button onClick={() => setAddress('Radauti')}>Set Address</button>
     </Card>
   );
 };
+
+//========================================== VS ====================================
 
 export class StateClassComponent extends React.Component {
   constructor() {

83cd93b6ebaf5109f25a152fa8a9a20695b97afb
Danutzz8 <Marius.Prutean@outlook.com>
2021-02-10 17:24:34 +0200

    hooks added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-import UseStateExample from './components/use-state-example/use-state-example.component';
+import Post from './components/post/post.component';
+import User from './components/user/user.component';
 
+//import UseStateExample from './components/use-state-example/use-state-example.component';
+//import UseEffectExample from './components/use-effect-example/use-effect-example.component';
 import './App.css';
 
 const App = props => {
-  return <UseStateExample />;
+  return <div>
+          <Post /><br/>
+          <User />
+        </div>
 };
 
 export default App;
